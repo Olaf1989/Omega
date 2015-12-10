99bd6eed57b76a20a5fb742ae57f8ce35681cd31
Olaf <olafvdb@hotmail.com>
2015-12-10 10:30:12 +0100

    Sorcery gem installed
    Back-up update in case something goes horribly wrong

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :courses
   root 'courses#home'
   # The priority is based upon order of creation: first created -> highest priority.
