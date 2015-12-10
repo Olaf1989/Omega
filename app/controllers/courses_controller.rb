78bfc4e129636ce0a2691e26b32f8b26f03cfca1
Olaf <olafvdb@hotmail.com>
2015-12-10 11:59:31 +0100

    Register/Login/Authentication

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
+  skip_before_action :require_login, only: [:index, :home, :show]
 
   # GET /courses
   # GET /courses.json
